cd599245bc15a93241f59cead07d5708879c2021
AcensJJ <uentosacens@gmail.com>
2019-02-06 15:22:09 +0100

    img modif config

--- src/Form/PostType.php
+++ src/Form/PostType.php
@@ -35,46 +35,38 @@ class PostType extends AbstractType
                     'maxlength' => 999
                     )
             ])
-            ->add('my_files', FileType::class, [
-                'required'   => false,
-                'mapped'   => false,
-                'multiple'   => true,
-                'empty_data' => '',
-                'label' => 'Ajouter une/des image(s)',
-                'constraints' => [
-                    // new Assert\File([ 
-                    //     'mimeTypes' => [
-                    //         'application/png',
-                    //         'application/jpg',
-                    //     ],
-                    //     'mimeTypesMessage' => 'Seulement les images .jpg ou .png',
-                    //     ])
-                        //  ,
-                    // new Assert\Image([ 
-                    //     'minWidth' => 200,
-                    //     'maxWidth' => 600,
-                    //     'minHeight' => 200,
-                    //     'maxHeight' => 600,
-                    //     ])
-                    ],
-                // 
-                // 
-                ])
-                // ->add('my_files', new Assert\File([
-                //     // 'class' => 'FileType',
-                //     // 'required'   => false,
-                //     // 'mapped'   => false,
-                //     // 'multiple'   => true,
-                //     // // 'empty_data' => '',
-                //     // 'label' => 'Ajouter une/des image(s)',
-                //     'mimeTypes' => [
-                //         'application/png',
-                //             'application/jpg',
-                //     ],
-                //     'mimeTypesMessage' => 'Seulement les images .jpg ou .png',   
-                //     ]) 
-                //     )
-
+                ->add('my_files', FileType::class, [
+                    'required'   => false,
+                    'mapped'   => false,
+                    'multiple'   => true,
+                    'empty_data' => '',
+                    'label' => 'Ajouter une/des image(s)',
+                    'constraints' => [new Assert\All([
+                        new Assert\File([ 
+                            'mimeTypes' => [
+                                'image/png',
+                                'image/jpg',
+                                'image/jpeg',
+                            ],
+                            'mimeTypesMessage' => 'Seulement les images .jpg, .jpeg ou .png',
+                            ]),
+                        new Assert\Image([ 
+                            'maxRatio' => 2,
+                            'maxRatioMessage' => 'Votre image ne peut pas être deux fois plus grande en hauteur / largeur',
+                            'minRatio' => 0.5,
+                            'minRatioMessage' => 'Votre image ne peut pas être deux fois plus grande en largeur / hauteur',
+                            'minWidth' => 200,
+                            'maxWidth' => 1200,
+                            'minHeight' => 200,
+                            'maxHeight' => 1200,
+                            'maxWidthMessage' => 'La largeur maximum d\'une image est de {{ max_width }}px, la votre fait {{ width }}px',
+                            'minWidthMessage' => 'La largeur minimum d\'une image est de {{ min_width }}px, la votre fait {{ width }}px',
+                            'maxHeightMessage' => 'La hauteur maximum d\'une image est de {{ max_height }}px, la votre fait {{ height }}px',
+                            'minHeightMessage' => 'La hauteur minimum d\'une image est de {{ min_height }}px, la votre fait {{ height }}px',
+                        ]),
+                    ])
+                ]
+            ])
         ;
     }
 
