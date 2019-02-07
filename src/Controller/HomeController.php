aae85cf97e4c4da04e4a4902ec91b4d5aa98a4a5
AcensJJ <uentosacens@gmail.com>
2019-02-07 22:42:23 +0100

    correction

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,16 +2,20 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="home")
      */
-    public function index()
+    public function index(UserInterface $user = null)
     {
+        if($user != null ) {
+            return $this->redirectToRoute('social');  
+        }
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
